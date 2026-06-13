2acd63724a0fe2ecac1ce6551cf7f2ffb2d1ac75
Yaswanth Reddy <165630157+yaswanth1815@users.noreply.github.com>
2026-06-13 12:51:53 +0530

    Hello Man

--- Two_Pointers/Patterns/Pattern3/Pattern3.py
+++ Two_Pointers/Patterns/Pattern3/Pattern3.py
@@ -1,15 +1,30 @@
+# k=4
+# num=[40,30,22,1,34,56,78,99]
+# summ=0
+# for i in range(k):
+#     summ+=num[i]
+# maxx=summ
+# left=k-1
+# right=len(num)-1
+
+# while(right>=k and left>=0):
+#     summ=summ-num[left]+num[right]
+#     maxx=max(maxx,summ)
+#     left-=1
+#     right-=1
+# print(maxx)
+
+from functools import reduce
 k=4
-num=[40,30,22,1,34,56,78,99]
-summ=0
-for i in range(k):
-    summ+=num[i]
-maxx=summ
+num=[40,30,22,1,11]
+summ=reduce(lambda x,y:x+y,num[0:k])
 left=k-1
 right=len(num)-1
+maxx=summ
 
-while(right>=k and left>=0):
+while(left>=0 and right>left):
     summ=summ-num[left]+num[right]
-    maxx=max(maxx,summ)
+    maxx=max(summ,maxx)
     left-=1
     right-=1
 print(maxx)
